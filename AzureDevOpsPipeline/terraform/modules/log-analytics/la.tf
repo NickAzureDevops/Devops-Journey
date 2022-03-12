cd60eaa5e9934009f7c7850237f0bf5b4b683bda
nicholaschangIT <nicholasc001@hotmail.com>
2022-03-12 01:22:50 +0000

    some fixes

--- AzureDevOpsPipeline/terraform/modules/log-analytics/la.tf
+++ AzureDevOpsPipeline/terraform/modules/log-analytics/la.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "kubernetes_resource_group" {
   }
 }
 
-resource "azurerm_log_analytics_workspace" "Log_Analytics_WorkSpace1" {
+resource "azurerm_log_analytics_workspace" "Log_Analytics_WorkSpace" {
     # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
     name                = var.log_analytics_workspace_name
     location            = var.location
